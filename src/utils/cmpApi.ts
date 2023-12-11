d9ed9565bea933c26c0ba655883604a645f20b1c
Ganivada Mouli <vishnumouli0@gmail.com>
2023-12-11 16:51:16 +0530

    fix 7

--- src/utils/cmpApi.ts
+++ src/utils/cmpApi.ts
@@ -3,19 +3,15 @@ const nseIndia = new NseIndia();
 
 export async function fetchCurrentMarketPrice(symbol: string): Promise<number> {
   try {
-    console.log("Hitting the fetchCurrentMarketPrice Function with ", symbol)
     const details = await nseIndia.getEquityDetails(symbol);
-    console.log("details",details)
 
     if (details.priceInfo && typeof details.priceInfo.lastPrice === "number") {
       return details.priceInfo.lastPrice;
-      console.log("PRICE",details.priceInfo.lastPrice)
     } else {
       console.log("Error")    
       throw new Error("Invalid price information");
     }
   } catch (err) {
-    console.log("ERR")
     console.error(err);
     throw err; // rethrow the error or handle it accordingly
   }
